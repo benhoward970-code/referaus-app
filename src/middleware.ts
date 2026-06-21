42e0f2d8c9748aaa25af7b1c2adbd9d16297e1ee
BenH <benhoward970@gmail.com>
2026-06-21 20:01:45 +1000

    Delete src/middleware.ts

--- src/middleware.ts
+++
@@ -1,4 +0,0 @@
-// This file has been migrated to src/proxy.ts (Next.js 16+ convention).
-// Next.js 16 deprecated middleware.ts in favour of proxy.ts.
-// See: https://nextjs.org/docs/messages/middleware-to-proxy
-export {};
